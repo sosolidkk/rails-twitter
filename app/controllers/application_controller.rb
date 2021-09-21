bc1dc243c5f0a37cd8003a6b0c7f08c5d9c8ccd8
sosolidkk <pedrochaveslimas3@gmail.com>
2021-09-21 01:10:38 -0300

    Fix wrong error message on sign_out for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio, :email, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :bio, :email, :password, :password_confirmation, :current_password])
