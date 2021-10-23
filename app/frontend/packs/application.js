149e0331f0666882e9165559c1696632a1b1045b
sosolidkk <pedrochaveslimas3@gmail.com>
2021-10-23 16:56:08 -0300

    Fix javascript not working on rails 6 app

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,8 +6,10 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import '../js/bootstrap_js_files.js'
 import "channels"
+
+import "bootstrap"
+import "../packs/application.scss"
 
 Rails.start()
 Turbolinks.start()
