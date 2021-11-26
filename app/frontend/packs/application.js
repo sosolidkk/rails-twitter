f8f0770e573fea4da18b71140e695a8635aeeca8
sosolidkk <pedrochaveslimas3@gmail.com>
2021-11-26 11:53:06 -0300

    Temporary disabling turbolinks

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -14,6 +14,6 @@ import "../packs/application.scss"
 import "../packs/notification.js"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 window.$ = window.JQuery = JQuery;
