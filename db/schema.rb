541259a8f13e4db2c3f1037478c609f414435a9e
sosolidkk <pedrochaveslimas3@gmail.com>
2022-03-08 20:48:17 -0300

    Add trackable info related to devise usage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_013030) do
+ActiveRecord::Schema.define(version: 2022_03_08_233727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(version: 2021_11_05_013030) do
     t.string "username"
     t.string "bio"
     t.string "fake_username"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
